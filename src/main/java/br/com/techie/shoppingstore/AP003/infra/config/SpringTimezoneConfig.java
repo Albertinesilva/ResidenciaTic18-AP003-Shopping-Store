9132eeba2a0b68c91447de051e1eeecb96beb59e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-14 21:03:22 -0300

    Application.properties, Pom.xml e SpringTimezoneConfig

---
+++ src/main/java/br/com/techie/shoppingstore/AP003/infra/config/SpringTimezoneConfig.java
@@ -0,0 +1,15 @@
+package br.com.techie.shoppingstore.AP003.infra.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimezoneConfig {
+
+    @PostConstruct
+    public void timezoneConfig() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
+}
