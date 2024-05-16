3691ead36b6a955adde213a90adce08ac63fa21e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-05-15 21:44:44 -0300

    ProdutoForm

--- src/main/java/br/com/techie/shoppingstore/AP003/dto/form/ProdutoForm.java
+++ src/main/java/br/com/techie/shoppingstore/AP003/dto/form/ProdutoForm.java
@@ -1,5 +1,28 @@
 package br.com.techie.shoppingstore.AP003.dto.form;
 
-public class ProdutoForm {
-  
+import lombok.Data;
+
+import java.io.Serializable;
+
+import br.com.techie.shoppingstore.AP003.model.AtributosServidor;
+import br.com.techie.shoppingstore.AP003.model.Categoria;
+import br.com.techie.shoppingstore.AP003.model.ItemCarrinho;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoForm implements Serializable{
+  private static final long serialVersionUID = 1L;
+
+  private Long id;
+  private String nome;
+  private Long preco;
+  private String descricao;
+  private int qtd_estoque;
+  private String url_imagem;
+  private AtributosServidor atributosServidor;
+  private ItemCarrinho itemCarrinho;
+  private Categoria categoria;
 }
