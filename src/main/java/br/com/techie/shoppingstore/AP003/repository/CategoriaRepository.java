7ecab81fb5915f7ceeaeebfb1416e8eb0eeb9dfb
leogmc <ifbaianoleo@gmail.com>
2024-05-15 21:17:29 -0300

    Entidades, repositórios e testes concluidos

--- src/main/java/br/com/techie/shoppingstore/AP003/repository/CategoriaRepository.java
+++ src/main/java/br/com/techie/shoppingstore/AP003/repository/CategoriaRepository.java
@@ -2,8 +2,8 @@ package br.com.techie.shoppingstore.AP003.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.techie.shoppingstore.AP003.model.Carrinho;
+import br.com.techie.shoppingstore.AP003.model.Categoria;
 
-public interface CategoriaRepository extends JpaRepository <Carrinho, Long> {
+public interface CategoriaRepository extends JpaRepository <Categoria, Long> {
 
 }
